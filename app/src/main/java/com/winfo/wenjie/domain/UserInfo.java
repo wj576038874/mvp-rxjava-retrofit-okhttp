5551a76eb9548cbcc49ef5283c750db0786de78b
wenjie <https://github.com/wj576038874/mvp-rxjava-retrofit-okhttp.git>
2017-06-01 10:54:38 +0800

    add

---
+++ app/src/main/java/com/winfo/wenjie/domain/UserInfo.java
@@ -0,0 +1,302 @@
+package com.winfo.wenjie.domain;
+
+/**
+ * @ProjectName: MvpRxjavaRetrofitDemo
+ * @PackageName: com.winfo.wenjie.domain
+ * @FileName: com.winfo.wenjie.domain.UserInfo.java
+ * @Author: wenjie
+ * @Date: 2016-12-29 16:26
+ * @Description:
+ * @Version:
+ */
+public class UserInfo {
+
+    private long yhxxid;
+    /**
+     * 状态标示
+     */
+    private String ztbs;
+    /**
+     * 身份特征码
+     */
+    private String sftzm;
+    /**
+     * 用户姓名
+     */
+    private String yhxm;
+    /**
+     * 所属部门
+     */
+    private String ssbm;
+    /**
+     * 民族
+     */
+    private String minzu;
+    /**
+     * 学历
+     */
+    private String xueli;
+    /**
+     * 职称
+     */
+    private String zhicheng;
+    /**
+     * 职务
+     */
+    private String zhiwu;
+    /**
+     * 地址
+     */
+    private String dizhi;
+    /**
+     * 邮编
+     */
+    private String youbian;
+    /**
+     * 电子邮件
+     */
+    private String youjian;
+    /**
+     * 电话
+     */
+    private String dianhua;
+    /**
+     * 移动电话
+     */
+    private String shouji;
+    /**
+     * 性别
+     */
+    private String xingbie;
+    /**
+     * 政治面貌
+     */
+    private String zzmm;
+    /**
+     * 用户密码
+     */
+    private String yhmm;
+    /**
+     * 机构代码
+     */
+    private String jgdm;
+
+    /**
+     * 机构名称
+     */
+    private String jgdmName;
+
+
+    /**
+     * 证件号码
+     */
+    private String zjhm;
+    /**
+     * 视频会议账号
+     */
+    private String sphyzh;
+    /**
+     * 视频会议密码
+     */
+    private String sphymm;
+    /**
+     * 身份证号码
+     */
+    private String idcard;
+    /**
+     * 用户海图操作
+     */
+    private String htcz;
+
+    public long getYhxxid() {
+        return yhxxid;
+    }
+
+    public void setYhxxid(long yhxxid) {
+        this.yhxxid = yhxxid;
+    }
+
+    public String getZtbs() {
+        return ztbs;
+    }
+
+    public void setZtbs(String ztbs) {
+        this.ztbs = ztbs;
+    }
+
+    public String getSftzm() {
+        return sftzm;
+    }
+
+    public void setSftzm(String sftzm) {
+        this.sftzm = sftzm;
+    }
+
+    public String getYhxm() {
+        return yhxm;
+    }
+
+    public void setYhxm(String yhxm) {
+        this.yhxm = yhxm;
+    }
+
+    public String getSsbm() {
+        return ssbm;
+    }
+
+    public void setSsbm(String ssbm) {
+        this.ssbm = ssbm;
+    }
+
+    public String getMinzu() {
+        return minzu;
+    }
+
+    public void setMinzu(String minzu) {
+        this.minzu = minzu;
+    }
+
+    public String getXueli() {
+        return xueli;
+    }
+
+    public void setXueli(String xueli) {
+        this.xueli = xueli;
+    }
+
+    public String getZhicheng() {
+        return zhicheng;
+    }
+
+    public void setZhicheng(String zhicheng) {
+        this.zhicheng = zhicheng;
+    }
+
+    public String getZhiwu() {
+        return zhiwu;
+    }
+
+    public void setZhiwu(String zhiwu) {
+        this.zhiwu = zhiwu;
+    }
+
+    public String getDizhi() {
+        return dizhi;
+    }
+
+    public void setDizhi(String dizhi) {
+        this.dizhi = dizhi;
+    }
+
+    public String getYoubian() {
+        return youbian;
+    }
+
+    public void setYoubian(String youbian) {
+        this.youbian = youbian;
+    }
+
+    public String getYoujian() {
+        return youjian;
+    }
+
+    public void setYoujian(String youjian) {
+        this.youjian = youjian;
+    }
+
+    public String getDianhua() {
+        return dianhua;
+    }
+
+    public void setDianhua(String dianhua) {
+        this.dianhua = dianhua;
+    }
+
+    public String getShouji() {
+        return shouji;
+    }
+
+    public void setShouji(String shouji) {
+        this.shouji = shouji;
+    }
+
+    public String getXingbie() {
+        return xingbie;
+    }
+
+    public void setXingbie(String xingbie) {
+        this.xingbie = xingbie;
+    }
+
+    public String getZzmm() {
+        return zzmm;
+    }
+
+    public void setZzmm(String zzmm) {
+        this.zzmm = zzmm;
+    }
+
+    public String getYhmm() {
+        return yhmm;
+    }
+
+    public void setYhmm(String yhmm) {
+        this.yhmm = yhmm;
+    }
+
+    public String getJgdm() {
+        return jgdm;
+    }
+
+    public void setJgdm(String jgdm) {
+        this.jgdm = jgdm;
+    }
+
+    public String getJgdmName() {
+        return jgdmName;
+    }
+
+    public void setJgdmName(String jgdmName) {
+        this.jgdmName = jgdmName;
+    }
+
+    public String getZjhm() {
+        return zjhm;
+    }
+
+    public void setZjhm(String zjhm) {
+        this.zjhm = zjhm;
+    }
+
+    public String getSphyzh() {
+        return sphyzh;
+    }
+
+    public void setSphyzh(String sphyzh) {
+        this.sphyzh = sphyzh;
+    }
+
+    public String getSphymm() {
+        return sphymm;
+    }
+
+    public void setSphymm(String sphymm) {
+        this.sphymm = sphymm;
+    }
+
+    public String getIdcard() {
+        return idcard;
+    }
+
+    public void setIdcard(String idcard) {
+        this.idcard = idcard;
+    }
+
+    public String getHtcz() {
+        return htcz;
+    }
+
+    public void setHtcz(String htcz) {
+        this.htcz = htcz;
+    }
+}
