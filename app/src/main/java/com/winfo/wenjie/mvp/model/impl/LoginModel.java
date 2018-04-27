d9919463e05909add67cbef3c4f514cfcc0d0711
wj576038874 <1rujiwang>
2018-04-27 15:19:58 +0800

    rxjava2

--- app/src/main/java/com/winfo/wenjie/mvp/model/impl/LoginModel.java
+++ app/src/main/java/com/winfo/wenjie/mvp/model/impl/LoginModel.java
@@ -12,14 +12,20 @@ import com.winfo.wenjie.request.ApiService;
 import com.winfo.wenjie.request.OkHttpUtils;
 import com.winfo.wenjie.request.RequestSubscriber;
 import com.winfo.wenjie.utils.CacheUtil;
+
 import java.util.List;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
+import retrofit2.Response;
 
 /**
  * ProjectName: MvpRxjavaRetrofitDemo
@@ -33,17 +39,23 @@ public class LoginModel implements ILoginModel {
 
     @Override
     public void login(String client_id, String client_secret, String grant_type, String username, String password, final OnLoadDatasListener<Token> onLoadDatasListener) {
-         /*
+        /*
          * 被订阅者
          */
-        Observable<Token> observable = OkHttpUtils.getRetrofit().create(ApiService.class).getToken(client_id, client_secret, grant_type, username, password);
+        Observable<Response<Token>> observable = OkHttpUtils.getRetrofit().create(ApiService.class).getToken(client_id, client_secret, grant_type, username, password);
         /*
          * 订阅者
          */
-        Observer<Token> subscriber = new RequestSubscriber<Token>() {
+        Observer<Response<Token>> subscriber = new RequestSubscriber<Response<Token>>() {
             @Override
-            protected void onSuccess(Token token) {
-                onLoadDatasListener.onSuccess(token);
+            protected void onSuccess(Response<Token> response) {
+                if (response.isSuccessful()) {
+                    onLoadDatasListener.onSuccess(response.body());
+                } else if (response.code() == 401) {
+                    onLoadDatasListener.onFailure("用户名或密码错误");
+                } else {
+                    onLoadDatasListener.onFailure("登录失败");
+                }
             }
 
             @Override
@@ -58,32 +70,48 @@ public class LoginModel implements ILoginModel {
     }
 
     @Override
-    public void getMe(final OnLoadDatasListener<UserDetail> onLoadDatasListener) {
+    public void getMe(String client_id, String client_secret, String grant_type, String username, String password, final OnLoadDatasListener<UserDetail> onLoadDatasListener) {
         /*
         先通过用户名和密码获取到token  再继续用token作为参数，去请求用户信息，这样就达到了嵌套请求
         第一个请求的返回数据，作为第二个请求的参数
          */
-        Observable<UserDetail> observable = OkHttpUtils.getRetrofit().create(ApiService.class).getToken("", "", "password", "wj576038874", "1rujiwang")
-                .flatMap(new Function<Token, ObservableSource<UserDetail>>() {
+        Observable<Response<UserDetail>> observable = OkHttpUtils.getRetrofit().create(ApiService.class).getToken(client_id, client_secret, grant_type, username, password)
+                .flatMap(new Function<Response<Token>, ObservableSource<Response<UserDetail>>>() {
                     @Override
-                    public Observable<UserDetail> apply(Token token) throws Exception{
+                    public ObservableSource<Response<UserDetail>> apply(Response<Token> response) throws Exception {
                         //第一个“登录请求”完成之后 会返回token信息，我们把token保存在本地，以便于第二个“获取用户信息”的请求可以从
                         //本地获取到token作为请求所需要的参数
-                        CacheUtil cacheUtil = new CacheUtil(MyApplication.getContext());
-                        cacheUtil.saveToken(token);
-                        return OkHttpUtils.getRetrofit().create(ApiService.class).getMe();
+                        if (response.isSuccessful()) {
+                            CacheUtil cacheUtil = new CacheUtil(MyApplication.getContext());
+                            Token token = response.body();
+                            assert token != null;
+                            cacheUtil.saveToken(token);
+                            return OkHttpUtils.getRetrofit().create(ApiService.class).getMe();
+                        }
+                        //如果用户登录出错，没有获取到token的话，那么我们就把用户登录的请求返回的response
+                        //封装到 Response<UserDetail> 然后发射出去，进行code的值判断显示错误信息
+                        ResponseBody responseBody = response.errorBody();
+                        assert responseBody != null;
+                        Response<UserDetail> userDetailResponse = Response.error(response.code(), responseBody);
+                        return Observable.just(userDetailResponse);
                     }
-                }).map(new Function<UserDetail, UserDetail>() {
+                }).map(new Function<Response<UserDetail>, Response<UserDetail>>() {
                     @Override
-                    public UserDetail apply(UserDetail userDetail) throws Exception {
-                        return userDetail;
+                    public Response<UserDetail> apply(Response<UserDetail> userDetailResponse) throws Exception {
+                        return userDetailResponse;
                     }
                 });
 
-        Observer<UserDetail> subscriber = new RequestSubscriber<UserDetail>() {
+        Observer<Response<UserDetail>> subscriber = new RequestSubscriber<Response<UserDetail>>() {
             @Override
-            protected void onSuccess(UserDetail userDetail) {
-                onLoadDatasListener.onSuccess(userDetail);
+            protected void onSuccess(Response<UserDetail> userDetailResponse) {
+                if (userDetailResponse.isSuccessful()) {
+                    onLoadDatasListener.onSuccess(userDetailResponse.body());
+                } else if (userDetailResponse.code() == 401) {
+                    onLoadDatasListener.onFailure("用户名或密码错误");
+                } else {
+                    onLoadDatasListener.onFailure("获取用户信息失败");
+                }
             }
 
             @Override
@@ -99,16 +127,20 @@ public class LoginModel implements ILoginModel {
 
     @Override
     public void bebing(final OnLoadDatasListener<TopicsAndNews> onLoadDatasListener) {
-        Observable<List<Topic>> observable1 = OkHttpUtils.getRetrofit().create(ApiService.class).loadTopicList(1);
-        Observable<List<New>> observable2 = OkHttpUtils.getRetrofit().create(ApiService.class).loadNewsList(1);
+        Observable<Response<List<Topic>>> observable1 = OkHttpUtils.getRetrofit().create(ApiService.class).loadTopicList(1);
+        Observable<Response<List<New>>> observable2 = OkHttpUtils.getRetrofit().create(ApiService.class).loadNewsList(1);
 
-        Observable<TopicsAndNews> observable = Observable.zip(observable1, observable2, new BiFunction<List<Topic>, List<New>, TopicsAndNews>() {
+        Observable<TopicsAndNews> observable = Observable.zip(observable1, observable2, new BiFunction<Response<List<Topic>>, Response<List<New>>, TopicsAndNews>() {
 
             @Override
-            public TopicsAndNews apply(List<Topic> topics, List<New> news) throws Exception {
+            public TopicsAndNews apply(Response<List<Topic>> topics, Response<List<New>> news) throws Exception {
                 TopicsAndNews topicsAndNews = new TopicsAndNews();
-                topicsAndNews.setNews(news);
-                topicsAndNews.setTopics(topics);
+                if (news.isSuccessful()) {
+                    topicsAndNews.setNews(news.body());
+                }
+                if (topics.isSuccessful()) {
+                    topicsAndNews.setTopics(topics.body());
+                }
                 return topicsAndNews;
             }
         });
