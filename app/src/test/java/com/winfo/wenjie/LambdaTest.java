c2a68bb327732d9a350db6fc8cb294c8018955f1
wj576038874 <1rujiwang>
2018-04-28 15:24:03 +0800

    rxjava2

--- app/src/test/java/com/winfo/wenjie/LambdaTest.java
+++ app/src/test/java/com/winfo/wenjie/LambdaTest.java
@@ -149,6 +149,9 @@ public class LambdaTest {
     public void testLambda10() {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         funtion(list, i -> "function" + i);
+
+
+
     }
 
     private void funtion(List<Integer> list, Function<Integer, String> function) {
